27bebbd18bc0d75070b43d2d2f99f3e77b62a613
Yohanr19 <yohanreyes@outlook.com>
2022-04-08 15:38:11 -0400

    Package draft definition

--- convertions.go
+++ convertions.go
@@ -1,2 +1,18 @@
 package convertions
 
+
+/* 
+Takes value and show 
+Weight in Kilograms cnnverted to Lb 
+Weight in lb to Kilograms
+Temperature in Celcius to Fahrenheit 
+Temperature in Farenheit to Celcius
+Distance in meters to yards
+Distance in yards to meters 
+Distance in meters to foot
+Distance in foot to meters
+Distance in foot to yards
+Distance in yards to foot
+Volume in Liters to Gallons
+Volume to Gallons in Liters
+*/
