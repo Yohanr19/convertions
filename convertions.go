3cacf5dc0f29e6cdc27351cdd62743d890937e7a
Yohanr19 <yohanreyes@outlook.com>
2022-05-09 17:53:35 -0400

    Celcius flag builder

--- convertions.go
+++ convertions.go
@@ -16,6 +16,7 @@
 package convertions
 
 import (
+	"flag"
 	"fmt"
 )
 
@@ -93,9 +94,9 @@ func (f Foots) String() string      { return fmt.Sprintf("%.4fft", f) }
 func (l Liters) String() string     { return fmt.Sprintf("%.4fL", l) }
 func (g Gallons) String() string    { return fmt.Sprintf("%.4fgal(US)", g) }
 
-type CelsiusFlag struct{ Celsius }
+type celsiusFlag struct{ Celsius }
 
-func (f *CelsiusFlag) Set(s string) error {
+func (f *celsiusFlag) Set(s string) error {
 	var unit string
 	var value float64
 	fmt.Sscanf(s, "%f%s", &value, &unit) // no error check needed
@@ -107,7 +108,17 @@ func (f *CelsiusFlag) Set(s string) error {
 		f.Celsius = TFtoC(Fahrenheit(value))
 		return nil
 	case "K", "°K":
-	f.Celsius = KToC(Kelvin(value))
-	return nil
+		f.Celsius = KToC(Kelvin(value))
+		return nil
 	}
-	return fmt.Errorf("invalid temperature %q", s) }
+	return fmt.Errorf("invalid temperature %q", s)
+}
+
+// CelsiusFlag defines a Celsius flag with the specified name,
+// default value, and usage, and returns the address of the flag variable.
+// The flag argument must have a quantity and a unit, e.g., "100C".
+func CelsiusFlag(name string, value Celsius, usage string) *Celsius {
+	f := celsiusFlag{value}
+	flag.CommandLine.Var(&f, name, usage)
+	return &f.Celsius
+}
